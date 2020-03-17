17867b88d7a14595f456bd26fd00810f15e7975a
Alexander <alexander.hritsun@gmail.com>
2020-03-17 18:10:14 +0200

    Map

---
+++ Arrays methods/Map.js
@@ -0,0 +1,9 @@
+/*
+let result = arr.map(function(item, index, array) {
+     returns new value for the array
+});*/
+
+let numbers = [4,36,64,100,25];
+console.log(numbers.map(Math.sqrt)); // 2, 6, 8, 10, 5
+
+
