8a60169a7076ded53c44ca25217202f406e2d7e6
Alexander <alexander.hritsun@gmail.com>
2020-03-17 17:49:23 +0200

    Search methods: find, filter

--- Arrays methods/SearchMethods.js
+++ Arrays methods/SearchMethods.js
@@ -8,3 +8,20 @@ console.log(array.indexOf(2)); //1
 console.log(array.lastIndexOf(1)); //0
 console.log(array.lastIndexOf(5)); //-1
 console.log(array.includes(3)); //true
+
+//find - search elements according to special conditions, search breaks when the first elements found
+
+function isPrime(element, index, array) {
+    var start = 2;
+    while (start <= Math.sqrt(element)) {
+        if (element % start++ < 1) {
+            return false;
+        }
+    }
+    return element > 1;
+}
+
+console.log([4, 6, 8, 12].find(isPrime)); //undefined
+console.log([4, 5, 8, 7].find(isPrime)); // 5
+//filter works like find but returns all elements
+console.log([4, 5, 8, 7].filter(isPrime)); //5,7
