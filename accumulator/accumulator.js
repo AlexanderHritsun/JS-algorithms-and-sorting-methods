ddcbe24979462244ad9437e261066d85d00b0405
Alexander Hritsun <ahritsun@opengov.com>
2020-12-13 18:09:42 +0200

    add accumulator function

---
+++ accumulator/accumulator.js
@@ -0,0 +1,23 @@
+/*
+Напишите функцию-конструктор Accumulator(startingValue).
+
+Объект, который она создаёт, должен уметь следующее:
+
+Хранить «текущее значение» в свойстве value. Начальное значение устанавливается в аргументе конструктора startingValue.
+Метод read() использует prompt для получения числа и прибавляет его к свойству value.
+Таким образом, свойство value является текущей суммой всего, что ввёл пользователь при вызовах метода read(), с учётом начального значения startingValue.
+*/
+
+function Accumulator(startingValue) {
+    this.value = startingValue;
+    this.read = function () {
+        this.value += +prompt('Сколько нужно добавить?', 0);
+    }
+}
+
+let test = new Accumulator(1);
+test.read();
+test.read();
+alert(test.value);
+
+
